9cc05886051b58f85d4adfd5cda29338158c2424
Annie  Nguyen <annienguyen@Annies-MacBook-Air.local>
2017-11-01 12:11:18 -0700

    send to git

--- stage-3/src/redux/store.js
+++ stage-3/src/redux/store.js
@@ -1,4 +1,7 @@
+import { createStore } from 'redux';
+import { applyMiddleware } from 'redux';
 import reducer from './reducer';
 import reduxPromiseMiddleware from 'redux-promise-middleware';
+
 
 export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
